9aa0078076367257e4808bb8a97b7400d86e373a
haripalbaluja <haripalbaluja.208@gmail.com>
2020-09-02 18:22:12 +0530

    change links

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -14,13 +14,13 @@ class NavBar extends Component {
                    <div className="header">
                    
                         <div className="heading">
-                            <Link to="/"  style={{ textDecoration: 'none', color: 'black' }}> 
+                            <Link to="/photography"  style={{ textDecoration: 'none', color: 'black' }}> 
                             <p> HARIPAL BALUJA </p>
                             </Link>
                         </div>
                         
                         <div className="subheading">
-                            <Link to="/"  style={{ textDecoration: 'none', color: 'black' }}>
+                            <Link to="/photography"  style={{ textDecoration: 'none', color: 'black' }}>
                             <p>PHOTOGRAPHY</p>
                             </Link>
                         </div>
@@ -45,11 +45,11 @@ class NavBar extends Component {
                         <Nav.Link  href="abstract"><h4 className="navlinks">ABSTRACT</h4></Nav.Link>
                         </Nav>    */}
                         <Nav className="ml-auto mr-auto">
-                        <Link to='/photography/abstract' href="abstract"><h4 className="navlinks">ABSTRACT</h4></Link>
-                        <Link to='/photography/scenic'><h4 className="navlinks">SCENIC</h4></Link>
-                        <Link to='/photography/street'><h4 className="navlinks">STREET</h4></Link>
-                        <Link to='/photography/contact'><h4 className="navlinks">CONTACT</h4></Link>
-                        <Link to='/photography/about'><h4 className="navlinks">ABOUT</h4></Link>
+                        <Link to='/photography/abstract' style={{ textDecoration: 'none', color: 'black' }}><h4 className="navlinks">ABSTRACT</h4></Link>
+                        <Link to='/photography/scenic'   style={{ textDecoration: 'none', color: 'black' }}><h4 className="navlinks">SCENIC</h4></Link>
+                        <Link to='/photography/street'   style={{ textDecoration: 'none', color: 'black' }}><h4 className="navlinks">STREET</h4></Link>
+                        <Link to='/photography/contact'  style={{ textDecoration: 'none', color: 'black' }}><h4 className="navlinks">CONTACT</h4></Link>
+                        <Link to='/photography/about'    style={{ textDecoration: 'none', color: 'black' }}><h4 className="navlinks">ABOUT</h4></Link>
                         
                        
                     </Nav>
