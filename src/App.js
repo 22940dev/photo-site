6e59a818760980ec91792155e90d231a0c5e186a
haripalbaluja <haripalbaluja.208@gmail.com>
2020-09-02 20:56:47 +0530

    change links

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ class App extends Component {
   render () {
     return (
       <div className="app">
-        <BrowserRouter>
+        <HashRouter>
         <Layout></Layout>
     
         <Switch>  
@@ -35,7 +35,7 @@ class App extends Component {
          
 
 
-        </BrowserRouter>
+        </HashRouter>
 
       </div>
     );
